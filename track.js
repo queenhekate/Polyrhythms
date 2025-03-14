5e1dc924050b34c22fcac1cadef35ae999d61f1e
Johanna Schnell <johannaschnell@Johannas-Air.lan>
2025-03-13 19:29:58 -0700

    final commit

--- track.js
+++ track.js
@@ -1,21 +1,30 @@
 class Track {
-    constructor(center, radius) {
+    constructor(center, radius, hue) {
         this.center = center;
         this.radius = radius;
+        this.hue = hue;
+        this.period = Math.PI;
     }
 
     getPosition(offset) {
         return {
-            x: this.center.x + this.radius * Math.cos(offset),
-            y: this.center.y - this.radius * Math.sin(offset)
+            x: this.center.x + Math.cos(offset) * this.radius,
+            y: this.center.y - Math.abs(Math.sin(offset)) * this.radius,
+            round: Math.floor(offset / this.period),
+            progress: (offset % this.period) / this.period
         };
     }
 
     draw(ctx) {
         ctx.beginPath();
-        ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.strokeStyle = "white";
+        for (let a = 0; a < Math.PI * 2; a += 0.1) {
+            const pos = this.getPosition(a);
+            ctx.lineTo(
+               pos.x, pos.y
+            );
+        }
+        ctx.closePath();
+        ctx.strokeStyle = `hsl(${this.hue}, 100%, 50%)`;
         ctx.stroke();
     }
 }
